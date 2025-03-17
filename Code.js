bb254372a7577115e053b4ca6d683aca07b74c87
usmansaeed2001 <muawan2001@gmail.com>
2025-03-17 22:04:13 +0500

    Test change 1

--- Code.js
+++ Code.js
@@ -1,6 +1,6 @@
 function checkGoCardLess() {
   var recordId = 'recnPHGUAvVaYrwkg'; // Get the Record ID from AU2
-  //New comment
+  //New comment added
   if (!recordId) {
     Logger.log("Record ID is empty.");
     return false;
